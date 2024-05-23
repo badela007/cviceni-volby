a87a3cad286d164c5009f5634d9b2f2074221abb
Adéla Přibylová <adelpribyl@gmail.com>
2024-05-23 21:17:00 +0200

    Řešení

--- src/components/Candidate/index.jsx
+++ src/components/Candidate/index.jsx
@@ -1,11 +1,13 @@
 import './style.css';
 
-export const Candidate = ({ name, avatar }) => {
+export const Candidate = ({ name, avatar, id, onSelect }) => {
   return (
     <div className="candidate">
       <h3 className="candidate__name">{name}</h3>
       <img className="candidate__avatar" src={avatar} />
-      <button className="btn-vote">Vybrat</button>
+      <button className="btn-vote" onClick={() => onSelect(id)}>
+        Vybrat
+      </button>
     </div>
   );
 };
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -4,15 +4,19 @@ import './style.css';
 
 export const HomePage = () => {
   const [candidates, setCandidates] = useState([]);
-  const [president, setPresident] = useState(null);
+  const [selectedPresidentIndex, setSelectedPresidentIndex] = useState(null);
 
-  useEffect(() => setCandidates([
-    { name: "Ferdinand Mravenec", avatar: '/avatars/candidate01.png' },
-    { name: "Markéta Smetana", avatar: '/avatars/candidate02.png' },
-    { name: "Beáta Skočdopolová", avatar: '/avatars/candidate03.png' },
-    { name: "Lubomír Poňuchálek", avatar: '/avatars/candidate04.png' },
-  ]), []);
-  
+  useEffect(
+    () =>
+      setCandidates([
+        { name: 'Ferdinand Mravenec', avatar: '/avatars/candidate01.png' },
+        { name: 'Markéta Smetana', avatar: '/avatars/candidate02.png' },
+        { name: 'Beáta Skočdopolová', avatar: '/avatars/candidate03.png' },
+        { name: 'Lubomír Poňuchálek', avatar: '/avatars/candidate04.png' },
+      ]),
+    [],
+  );
+
   return (
     <div className="container">
       <div className="castle">
@@ -20,20 +24,27 @@ export const HomePage = () => {
         <div className="castle__body">
           <h1>Nový prezident</h1>
           <p className="castle__president">
-            {
-              president === null ? 'Vyberte svého kandidáta' : president
-            }
+            {selectedPresidentIndex === null ? (
+              'Vyberte svého kandidáta'
+            ) : (
+              <>
+                <img src={candidates[selectedPresidentIndex].avatar} />
+                <strong>{candidates[selectedPresidentIndex].name}</strong>
+              </>
+            )}
           </p>
         </div>
       </div>
-      
+
       <h2>Kandidáti</h2>
       <div className="candidate-list">
-        {candidates.map((c) => (
-          <Candidate 
+        {candidates.map((c, index) => (
+          <Candidate
             key={c.name}
-            name={c.name} 
-            avatar={c.avatar} 
+            id={index}
+            name={c.name}
+            avatar={c.avatar}
+            onSelect={setSelectedPresidentIndex}
           />
         ))}
       </div>
